7189662bb86b7f8768436383b356f742822955b5
datokldiashvili <davitkldiashvili2007@gmail.com>
2023-11-23 10:27:17 +0400

    __

---
+++ API/Data/Migrations/20231121145006_AddGmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Gmail");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gmail",
+                table: "Users",
+                newName: "UserName");
+        }
+    }
+}
